483ad2c0159baaeb127107595fd28940c7508500
Tithuthur <thurthur.petit.py@gmail.com>
2025-04-04 13:30:33 +0200

    mise en place de la trajectoire initiale de la balle

--- scripts/Ball.cs
+++ scripts/Ball.cs
@@ -3,14 +3,22 @@ using System;
 
 public partial class Ball : RigidBody2D
 {
+
     // Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
+		RigidBody2D Ball = GetNode<RigidBody2D>("Ball");
+		Position = GetNode<Marker2D>("BallStartPosition").Position;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Vector2 velocity = Vector2.Zero;
+		velocity.X -= 1;
+        velocity.Y -= 1;
+		velocity = velocity.Normalized() * (float)delta;
+        Position = velocity;
+		
 	}
 }
