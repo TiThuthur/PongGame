e5d65305523ce4b35f171a4d3f5961207019f8cc
Tithuthur <thurthur.petit.py@gmail.com>
2025-04-10 13:59:33 +0200

    ajout d'une animation explode qui ne fonctionne pas encore

--- scripts/Screen.cs
+++ scripts/Screen.cs
@@ -4,25 +4,39 @@ using System;
 public partial class Screen : Node2D
 {
     int[] score = [0, 0];
-
+	Vector2 screenSize;
 	public void OnDetectionLeftBodyEntered(Node2D body)
+	{
+		if (body == GetNode<Ball>("Ball"))
+		{
+			score[1] += 1;
+			body.GetNode<AnimationPlayer>("AnimationPlayer").Play("explode");
+			body.Position = body.Position;
+			body.Free();
+			GetNode<Timer>("StartTimer").Start();
+			GetNode<AnimationPlayer>("Ball/");
+		}
+	}
+	public void OnDetectionRightBodyEntered(Node2D body)
 	{
 		if (body == GetNode<CharacterBody2D>("Ball"))
 		{
-			score[1] += 1;
+			score[0] += 1;
 			body.Free();
+			GetNode<Timer>("StartTimer").Start();
 		}
 	}
 	private void OnStartTimerTimeout()
 	{
 		PackedScene packedScene = GD.Load<PackedScene>("res://scenes/ball.tscn");
 		Ball ball = packedScene.Instantiate<Ball>();//instancie une nouvelle balle
+		ball.Position = new Vector2(screenSize.X/2, screenSize.Y/2);
 		AddChild(ball);//Crée la balle
 		GetNode<Opponent>("Opponent").SetTarget(ball);//attribut à l'opponenet la ball
 	}
 	public override void _Ready()
 	{
-		
+		screenSize = GetViewportRect().Size;
 	}
 
 	public override void _Process(double delta)
